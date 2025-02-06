a63b7fc5e92cdc784eccc0dff600e2776af9348d
Peter Svensson <psvensson@gmail.com>
2025-02-06 10:46:22 +0100

    simplified test utils a bit

--- src/PartitionLayer.js
+++ src/PartitionLayer.js
@@ -1,4 +1,4 @@
-
+const { Parser } = require('node-sql-parser');
 
 // Overview:
 // The PartitionLayer  is a service which receives commands from any other service in the system which need to run queries
@@ -40,25 +40,79 @@ module.exports = class PartitionLayer {
             );
             CREATE INDEX IF NOT EXISTS idx_query ON live_query_subscribers (query);
         `;
-        this.partitionGroup.db.exec(sqlStatement);
+        this.partitionGroup.executeQuery(sqlStatement);
     }
 
-    handlePartitionQuery(query) {
+    async handlePartitionQuery(query) {
         // Execute query in the partition group
-
-        // If the query resulted in database changes, we see if we have any live queries regsitered that matches the WHERE clause of the query
-
-        // If we have matches, add the subscribers found to the recipients.
-
-        // Send results to members of receipients list
+        const result = await this.partitionGroup.executeQuery(query);
+        // Analyze query to understand if it updates the table
+        const parser = new Parser();
+        const parsedQuery = parser.astify(query);
+        if (parsedQuery.type === 'insert' || parsedQuery.type === 'update' || parsedQuery.type === 'delete') {
+             // If the query resulted in database changes, we see if we have any live queries regsitered that matches the WHERE clause of the query
+            this.handleLiveQuerySubscribers(query);
+        }
+        return result
     }
 
-    handleRegisterLiveQuery(query) {
+    async handleLiveQuerySubscribers(query) {
+        const subscribers = this.findLiveQuerySubscribers(query);
+        // Send the result to the subscribers
+        await Promise.all(subscribers.forEach(subscriber => {
+            const columns = subscriber.columns.split(',');
+            const recipients = subscriber.recipients.split(',');
+            this.messageLayer.sendMessageToNode(result, recipients);
+        }));
+    }
+
+    async handleRegisterLiveQuery(recipients, query) {
         // Register the live query in the partition group
+        const columns = this.extractColumnNamesFromQuery(query);
+        // Insert the live query into the live_query_subscribers table
+        const sqlStatement = `INSERT INTO live_query_subscribers (id, columns, recipients) VALUES ('${query}', '${columns.join(',')}', '${recipients.join(',')}')`;
+        return this.partitionGroup.executeQuery(sqlStatement);
     }
 
-    handleDeregisterLiveQuery(query) {
+    async handleDeregisterLiveQuery(recepients, query) {
         // Deregister the live query in the partition group
+        const sqlStatement = `DELETE FROM live_query_subscribers WHERE id = '${query}'`;
+        return this.partitionGroup.executeQuery(sqlStatement);
     }
+
+    async findLiveQuerySubscribers(query) {
+        // Find all live query subscribers that match the WHERE clause of the query
+        const columns = this.extractColumnNamesFromQuery(query);
+        const sqlStatement = `SELECT * FROM live_query_subscribers WHERE columns = '${columns.join(',')}'`;
+        return this.partitionGroup.executeQuery(sqlStatement);
+    }
+
+    extractColumnNamesFromQuery(query)  {
+        const parser = new Parser();
+        const parsedQuery = parser.astify(query);
+        // The parsed query is a tree structure that we need to traverse to find the columns
+        // We can use a recursive function to find the columns
+        const columns = [];    
+       // Use the parsedQuery structure to etract the columns
+        const extractColumns = (node) => {
+            if (node.type === 'column_ref') {
+                columns.push(node.column);
+            }
+            if (node.type === 'select') {
+                node.columns.forEach(column => {
+                    extractColumns(column.expr);
+                });
+            }
+            if (node.type === 'from') {
+                node.from.forEach(from => {
+                    extractColumns(from);
+                });
+            }
+        }
+        extractColumns(parsedQuery);
+        return columns;
+    }
+
+    
 
 }
--- test/messagingLayer.test.js
+++ test/messagingLayer.test.js
@@ -15,40 +15,39 @@ this.messageLayer = new MessagingLayer(messageGroup);
 
 */
 
+let mockHostIP = 1;
+
 function createNode(args) {
-    const mockTransport = new MockTransport(args.externalAddress);
-    const raft = new MockRaftImplementation(args.raftNodeAddress, args.peerAddresses);
+    ;
+    const address = args.externalAddress || 'mock://1.1.1.'+(mockHostIP++);
+    const nodePort = args.nodePort || 2002;
+    const nodeAddress = address+':'+nodePort;
+    const mockTransport = new MockTransport(nodeAddress);
+
+    const messageGroupPort = args.messageGroupPort || 4711;    
+    const raftNodeAddress = nodeAddress+':'+messageGroupPort;    
+    const raft = new MockRaftImplementation(raftNodeAddress, args.peerAddresses || [])
+
     return new Node({
-        externalAddress: args.externalAddress,
+        externalAddress: nodeAddress,
+        existingNode: args.existingNode,
         transportLayer: mockTransport,
         raftImplementation: raft,
     });
 }
 
 
-test('Test MessageLayer', async () => {
-    const node1Address = "mock://11.22.33.44:2002";
-    const raftNode1Address = "mock://11.22.33.44:4711";
-    const peerAddresses = [];
-    
+test('Test MessageLayer', async () => {    
+    const peerAddresses = [];    
     const node1 = await createNode({
-        externalAddress: node1Address,
-        raftNodeAddress: raftNode1Address,
         peerAddresses: peerAddresses
     });
 
     console.log('------------------------------- test node1 is:')
-    console.dir(node1)
-
-    
-    const node2Address = "mock://77.88.99.11:2002";
-    const raftNode2Address = "mock://77.88.99.11:4711";
-    
+    console.dir(node1)    
     const node2 = await createNode({
-        externalAddress: node2Address,
-        raftNodeAddress: raftNode2Address,
         peerAddresses: peerAddresses,
-        existingNode: node1Address
+        existingNode: node1.externalAddress
     });
         
     
